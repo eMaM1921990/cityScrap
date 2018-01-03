96a8069a551faa052aecd4dd8747c2489d631276
ahmed_elemam <emam151987@gmail.com>
2018-01-03 20:47:57 +0200

    celery

--- cityScrapper/settings.py
+++ cityScrapper/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'cityScrapper.wsgi.application'
 
 DATABASES = {
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'city_scrapper',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'J&d5FHYe6wp6',
-    #     'HOST': 'localhost',  # '127.0.0.1'
-    #     'PORT': '5432',  # '5556'
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'city_scrapper',
+        'USER': 'postgres',
+        'PASSWORD': 'J&d5FHYe6wp6',
+        'HOST': 'localhost',  # '127.0.0.1'
+        'PORT': '5432',  # '5556'
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 
 }
 
