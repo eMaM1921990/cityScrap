1e3c30d98416b742141ead0aed63255fe61795f8
Ahmed Elemam <eMaM151987@gmail.com>
2018-01-07 23:35:15 +0200

    Update views.py

--- cityScrapperApp/views.py
+++ cityScrapperApp/views.py
@@ -113,7 +113,7 @@ def scrap(request):
         return HttpResponse(json.dumps(ret, ensure_ascii=False))
     else:
 
-        cities = City.objects.values('name').filter(name__startswith='N').distinct()
+        cities = City.objects.values('name').filter(name__startswith='O').distinct()
         print cities
         job_numbers = 0
         for city in cities:
